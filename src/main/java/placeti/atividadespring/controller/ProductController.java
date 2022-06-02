51e3c847e1b232ef9045e8125ef0630077d21976
Paulo Filho <paulomacieltorresfilho@gmail.com>
2022-06-01 23:39:04 -0300

    modelo e controller feitos

---
+++ src/main/java/placeti/atividadespring/controller/ProductController.java
@@ -0,0 +1,43 @@
+package placeti.atividadespring.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import placeti.atividadespring.domain.Product;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("shop")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> list() {
+        return ResponseEntity.ok(productService.listAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> findById(@PathVariable long id) {
+        return ResponseEntity.ok(productService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> save(@RequestBody ProductPostRequestBody productPostRequestBody) {
+        return ResponseEntity.created(productService.save(productPostRequestBody));
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> replace(@RequestBody ProductPutRequestBody productPutRequestBody) {
+        productService.replace(productPutRequestBody);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable long id) {
+        productService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
